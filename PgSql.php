3fdbfd2d03b0305a905213427db90e31f86be00c
CliffyA <>
2026-03-26 01:22:35 +1100

    Cleanup

--- PgSql.php
+++ PgSql.php
@@ -4,12 +4,7 @@
 		public function __construct($sAction)
 		{
 			parent::__construct($sAction);
-			$this->m_xFileArray = ProjectGen_ParseDirectory(dirname(__FILE__), "/\.h$|\.c$|\.hpp$|\.cpp$/");
-
-			$this->m_xFileArray = array_merge(
-				ProjectGen_ParseDirectory(dirname(__FILE__), "/\.h$|\.c$|\.hpp$|\.cpp$/"),
-				//ProjectGen_ParseDirectory(dirname(__FILE__) . "/../../Transpiled", "/\.hpp$|\.cpp$/")
-			);
+			$this->m_xFileArray = ProjectGen_ParseDirectory(dirname(__FILE__), ProjectGen_GetSourceRegex($sAction));
 		}
 
 		public function GetName() { return "PgSql"; }
@@ -19,36 +14,21 @@
 		public function GetIncludeDirectoryArray($sConfiguration, $sArchitecture)
 		{
 			$sArray = array();
-			// PostgreSQL (libpq) include path for libpq-fe.h
-			// Common locations: Debian/Ubuntu use /usr/include/postgresql; others may use /usr/include
 			if (is_dir("/usr/include/postgresql"))
 				$sArray[] = "/usr/include/postgresql";
 			else
 				$sArray[] = "/usr/include";
-			// LangShared InternalString for m_lastError
-			$sArray[] = dirname(__FILE__) . "/../LangShared/InternalString/CPP";
 			return $sArray;
 		}
 
 		public function GetDependancyArray()
 		{
-			$sArray = array(
-			//	"LangShared",
-			//	"ThirdParty",
-			//	"Package",
-			//	"Engine",
-			//	"Core",
+			if (ProjectGen_ActionIsCS($this->m_sAction))
+				return array("Npgsql_10.0.2");
+
+			return array(
+				"-lpq",
 			);
-
-			/*if ($this->m_sAction == ACTION_CMAKE)
-			{
-				$sArray = array_merge($sArray, array(
-					"X11",
-					"m",
-				));
-			}*/
-
-			return $sArray;
 		}
 	}
 ?>
